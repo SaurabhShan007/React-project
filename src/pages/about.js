83cbbb3994a0ab3aa6e1ba0a11ff5c2119fefac8
Saurabh Chaudhary <saurabh.shanu007@gmail.com>
2023-12-11 22:06:01 +0530

    add about page

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../components/Header';
+const AboutPage = () => {
+    return (   <div>
+        <Header/>
+        
+        <main>
+            <h1>Hello from about</h1>
+        </main>
+            </div>  );
+}
+ 
+export default AboutPage;
